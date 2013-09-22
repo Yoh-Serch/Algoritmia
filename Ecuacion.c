c6a7bc6a92cff19e5df8664ef48f95391575d113
Yoh Serch <serch.century.nymphetamine.999@gmail.com>
2013-09-21 22:04:20 -0500

    Se creo la funcion para pedir datos

--- Ecuacion.c
+++ Ecuacion.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-float dato(char);
+float dato(char *);
 float obtener_y();
 float obtener_x();
 void imprimir_resultado(float, float);
@@ -8,23 +8,29 @@ void imprimir_resultado(float, float);
 main()
 {
 	float A, B, C, D, E, F, y, x;
-	printf("Programa que calcula una ecuacion de la forma\nAx + By = C\nDx + Ey = F");
-	printf("Por favor ingrese los datos que se van pidiendo");
+	printf("Programa que calcula una ecuacion de la forma\nAx + By = C\nDx + Ey = F\n");
+	printf("Por favor ingrese los datos que se van pidiendo\n");
 	A = dato("A");
+	B = dato("B");
+	C = dato("C");
+	D = dato("D");
+	E = dato("E");
+	F = dato("F");
 }
 
-float dato(char constante)
+float dato(char *constante)
 {
 	float valor;
-	printf("Ingrese el valor de %c: ",constante);
+	printf("\tIngrese el valor de %c: ", *constante);
 	scanf("%f", &valor);
+	return valor;
 }
 
 float obtener_y()
-{}
+{
+
+}
 
 float obtener_x()
 {}
 
-void imprimir_resultado(float, float)
-{}
