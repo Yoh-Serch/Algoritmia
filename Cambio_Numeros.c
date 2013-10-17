c66cb900f788942ecab88acd87a3e25802cb2efc
Yoh Serch <serch.century.nymphetamine.999@gmail.com>
2013-10-17 18:44:24 -0500

    Se comienza el programa

---
+++ Cambio_Numeros.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void leer_dato(int*, char);
+void encontrar_mayor(int* int* int*);
+void encontrar_medio(int*, int*);
+void imprimir(int*, int*, int*);
+void validar_numero(int*);
+
+main()
+{
+	int mayor, medio, menor;
+	leer_datos(*mayor, '1');
+}
+
+
+
