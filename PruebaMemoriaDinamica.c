5d0bb1b8758161cee26223036ee483ce78369f10
Yoh Serch <serch.centuy.nymphetamine.999@gmail.com>
2013-11-28 00:17:20 -0600

    Se agrego sentencia exit() para terminar bien el programa

--- PruebaMemoriaDinamica.c
+++ PruebaMemoriaDinamica.c
@@ -5,24 +5,25 @@ void imprimir(int, double*);
 
 int main()
 {
-	double *add;
+	double *array;
 	int tamano;
 	printf("Ingrese el tamaño del arreglo:: ");
 	scanf("%i", &tamano);
-	add = (double*)malloc(tamano*sizeof(double));
-	if(add == NULL)
+	array = (double*)malloc(tamano*sizeof(double));
+	if(array == NULL)
 	{
-		printf("Error en la asignacion de memoria");
+		printf("Error en la asignacion de memoria\n");
+		exit(-1);
 	} else{
 		int i;
 
 		for(i=0;i<tamano;i++)
 		{
 			printf("%i.- Ingrese un numero:: ", i+1);
-			scanf("%lf", &add[i]);
+			scanf("%lf", &array[i]);
 		}
-		imprimir(tamano, add);
-		free(add);
+		imprimir(tamano, array);
+		free(array);
 		printf("Fin del programa.\n");
 	}
 }
